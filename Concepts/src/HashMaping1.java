0a61ecd5f73b8d35272d7c04ec0f8d1ddf0a7659
manasa <aslmanasa3@gmail.com>
2021-02-25 18:28:36 +0530

    frequency of an element

---
+++ Concepts/src/HashMaping1.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.*;
+public class HashMaping1
+{
+    public static void main(String [] args) {
+        int arr [] = {1,2,1,2,3,3,4,4,4};
+        HashMap<Integer,Integer> hm = new HashMap<Integer,Integer>();
+        for(int i =0;i< arr.length;i++)
+        {
+            Integer c = hm.get(arr[i]);
+            if (hm.get(arr[i]) == null)
+            {
+                hm.put(arr[i],1);
+            }
+            else
+                hm.put(arr[i],++c);
+        }
+        System.out.println("Frequency of the elements present");
+        for(Map.Entry m: hm.entrySet())
+        {
+            System.out.println("Key value "+m.getKey() + "-"+"Frequncey "+ m.getValue());
+        }
+    }
+}
