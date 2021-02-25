032e24cd83877d31b653b6f75c408d156d418dcf
manasa <aslmanasa3@gmail.com>
2021-02-25 18:21:58 +0530

    hash maps modify

--- Concepts/src/HashMaping.java
+++ Concepts/src/HashMaping.java
@@ -3,22 +3,20 @@ import java.util.*;
 
 public class HashMaping
 {
-    public static void main(String [] args)
-    {
-        int arr[] = { 10, 34, 5, 10, 3, 5, 10 };
-        HashMap<Integer,Integer> hm = new HashMap<>();
+    public static void main(String [] args) {
+        int arr [] = {1,2,1,2,3,3,4,4,4};
+        HashMap <Integer,Integer> hm = new HashMap<Integer,Integer>();
         for(int i =0;i< arr.length;i++)
         {
             Integer c = hm.get(arr[i]);
-            //System.out.print(c+" ");
-            if( c == null)
+            if (hm.get(arr[i]) == null)
+            {
                 hm.put(arr[i],1);
+            }
             else
                 hm.put(arr[i],++c);
         }
-        /*for (Map.Entry m:hm.entrySet())
-            System.out.println("Frequency of " + m.getKey() +
-                    " is " + m.getValue());*/
-        System.out.print(hm);
+        System.out.println(hm);
     }
+
 }
