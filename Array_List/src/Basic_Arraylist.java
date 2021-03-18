be713f8234ab8aff10a7d9d03c70e4de8115a499
manasa <aslmanasa3@gmail.com>
2021-03-18 15:46:00 +0530

    Arraylist

---
+++ Array_List/src/Basic_Arraylist.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Basic_Arraylist
+{
+    public static void main(String [] args)
+    {
+        ArrayList<Integer> a = new ArrayList<>();
+        a.add(3);
+        a.add(6);
+        a.add(9);
+        a.add(12);
+        System.out.println(a);
+    }
+}
