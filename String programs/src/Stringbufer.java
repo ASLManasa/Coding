6f9dab0833840181a0ad843adf4b437c21ea3b1e
ASLManasa <aslmanasa3@gmail.com>
2021-04-16 10:39:49 +0530

    String buffer functionalities

---
+++ String programs/src/Stringbufer.java
@@ -0,0 +1,23 @@
+/*  String buffer functionalities */
+
+public class Stringbufer
+{
+    public static void main(String[] args)
+    {
+        StringBuffer str = new StringBuffer("string");
+        System.out.println(str);
+        str.append(" Builder");
+        System.out.println(str);
+        str.insert(0 , "Hi ");
+        System.out.println(str);
+        str.replace(0,3,"Hey, ");
+        System.out.println(str);
+        str.delete(0,5);
+        System.out.println(str);
+        str.reverse();
+        System.out.println(str);
+
+
+
+    }
+}
