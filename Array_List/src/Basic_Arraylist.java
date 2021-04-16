15daf2c052c34210aac168dffba43589628d6f8f
ASLManasa <aslmanasa3@gmail.com>
2021-04-16 11:15:00 +0530

    collections

--- Array_List/src/Basic_Arraylist.java
+++ Array_List/src/Basic_Arraylist.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.*;
 
 public class Basic_Arraylist
 {
@@ -6,12 +8,14 @@ public class Basic_Arraylist
     {
         ArrayList<Integer> a = new ArrayList<>();
         a.add(3);
-        a.add(6);
-        a.add(9);
+        a.add(2);
+        a.add(91);
         a.add(12);
         System.out.println(a);
         System.out.println(a.get(0));
         a.set(0,4);
+        System.out.println(a);
+        Collections.sort(a);
         System.out.println(a);
     }
 }
