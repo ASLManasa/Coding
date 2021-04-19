368d6322db9c1115eac61ba572c95c5c784145ef
ASLManasa <aslmanasa3@gmail.com>
2021-04-19 22:36:07 +0530

    Binary Tree implementation - Not Completed

---
+++ Binary_Tree/src/Basic_Binary_Tree.java
@@ -0,0 +1,12 @@
+public class Basic_Binary_Tree
+{
+    public static void main(String [] args)
+    {
+        BinaryTree bt = new BinaryTree();
+        bt.root = new Node(1);
+        bt.root.left = new Node(2);
+        bt.root.right = new Node(2);
+
+
+    }
+}
