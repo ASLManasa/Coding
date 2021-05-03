c65fc8b161d723b80b3e47527f34b2ef2f538d9e
ASLManasa <aslmanasa3@gmail.com>
2021-05-03 17:02:55 +0530

    pattern finding in a string using rabin karp algo

---
+++ Algorithms/src/KarpAlgorithm.java
@@ -0,0 +1,14 @@
+public class KarpAlgorithm {
+    public final static int d = 256;
+    public static void search(String pat,String txt,int q)
+    {
+        
+    }
+
+    public static void main(String[] args) {
+        String txt = "GEEKS FOR GEEKS";
+        String pat = "GEEK";
+        int q = 101;
+        search(pat, txt, q);
+    }
+}
