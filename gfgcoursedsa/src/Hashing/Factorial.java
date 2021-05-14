6fe6266fb40127b6a9b68ab2533b6fbe50632310
ASLManasa <aslmanasa3@gmail.com>
2021-05-14 15:16:59 +0530

    Factorial_of_a_number

---
+++ gfgcoursedsa/src/Hashing/Factorial.java
@@ -0,0 +1,17 @@
+package Hashing;
+
+public class Factorial
+{
+    public static int fact(int x)
+    {
+        if(x==1|| x==0)
+            return 1;
+        else
+            return x*fact(x-1);
+    }
+    public static void main (String [] args)
+    {
+        int x =5;
+        System.out.println(fact(x));
+    }
+}
