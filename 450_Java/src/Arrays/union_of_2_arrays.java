59f9ea9d1e4ee32a10be65e1686732472bc2627d
ASLManasa <aslmanasa3@gmail.com>
2021-06-06 11:09:52 +0530

    union of two arrays

---
+++ 450_Java/src/Arrays/union_of_2_arrays.java
@@ -0,0 +1,37 @@
+package Arrays;
+/* Approach -1  time complexity - o(n+m)*/
+public class union_of_2_arrays {
+    public static void union(int arr1[], int arr2[]) {
+        int i = 0;
+        int j = 0;
+        int m = arr1.length;
+        int n = arr2.length;
+        while(i < m && j < n)
+        {
+            if(arr1[i] < arr2[j]) {
+                System.out.print(arr1[i++]+" ");
+
+            }
+            else if(arr2[j] < arr1[i]) {
+                System.out.print(arr2[j++]+" ");
+
+            }
+            else {
+                System.out.print(arr2[j++]+" ");
+                i++;
+
+            }
+
+        }
+        if ( i < m)
+            System.out.println(arr1[i++]);
+        if ( j < n)
+            System.out.println(arr2[j++]);
+    }
+
+    public static void main(String[] args) {
+        int arr1[] = new int[]{1, 2, 4, 5, 6};
+        int arr2[] = new int[]{2, 3, 5, 7};
+        union(arr1, arr2);
+    }
+}
