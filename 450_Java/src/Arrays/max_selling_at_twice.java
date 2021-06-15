daa933b1c1cb9312421f6fbd6691535c040a94ce
ASLManasa <aslmanasa3@gmail.com>
2021-06-15 19:09:55 +0530

    Find whether an array is subset of another array

---
+++ 450_Java/src/Arrays/max_selling_at_twice.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+public class max_selling_at_twice
+{
+    public static void profit(int arr[])
+    {
+
+    }
+    public static void main(String [] args)
+    {
+        int arr[] = new int[]{10, 22, 5, 75, 65, 80};
+        profit(arr);
+    }
+}
