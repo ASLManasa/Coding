c4c1ad21be4a3bd20f290c559f4d0159decc6384
ASLManasa <aslmanasa3@gmail.com>
2021-06-24 13:21:12 +0530

    Duplicates in a string

---
+++ 450_Java/src/Strings/String_duplicates.java
@@ -0,0 +1,52 @@
+package Strings;
+
+import java.util.Arrays;
+
+public class String_duplicates
+{
+    public static void duplicates(String s)
+    {
+       /* int count[] = new int[256];
+        for(int i =0;i<s.length();i++)
+        {
+            count[s.charAt(i)]++;
+        }
+        for(int i =0;i< count.length;i++)
+        {
+            if(count[i] >1)
+                System.out.print((char) i + " " + count[i]);
+        }*/
+        char ch [] = s.toCharArray();
+        int freq [] =new int[s.length()];
+        for(int i =0;i<s.length();i++) {
+            freq[i] = 1;
+            for (int j = i + 1; j < s.length(); j++) {
+
+                if (ch[i] == ch[j]) {
+                    freq[i]++;
+                    ch[j] = '0';
+                }
+            }
+
+        }
+       /* for(int i =0;i< freq.length;i++)
+        {
+            if(ch[i] != ' ' && ch[i] != '0')
+                System.out.println(ch[i] + "-" + freq[i]);
+        }*/
+        for(int i =0;i< freq.length;i++)
+        {
+            if(freq[i] >1) {
+                System.out.println(ch[i]);
+            }
+            else
+                continue;
+        }
+
+    }
+   public static void main(String [] args)
+   {
+       String s = "geeksforgeeks";
+       duplicates(s);
+   }
+}
