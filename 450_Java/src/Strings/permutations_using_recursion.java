08175c39482ada0a56951d9a00cd03b6740b335d
ASLManasa <aslmanasa3@gmail.com>
2021-06-28 11:54:07 +0530

    Permutations

---
+++ 450_Java/src/Strings/permutations_using_recursion.java
@@ -0,0 +1,23 @@
+package Strings;
+
+public class permutations_using_recursion {
+    public static void permute(String ques, String asf) {
+        if (ques.length() == 0) {
+            System.out.println(asf);
+            return;
+        }
+        for (int i = 0; i < ques.length(); i++) {
+            char ch = ques.charAt(i);
+            String lpart = ques.substring(0, i);
+            String rpart = ques.substring(i + 1);
+            String roq = lpart + rpart;
+            permute(roq, asf + ch);
+
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = "abc";
+        permute(s, " ");
+    }
+}
