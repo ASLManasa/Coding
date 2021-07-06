cb088231349cbc536b670b808dcdebd3b423330d
ASLManasa <aslmanasa3@gmail.com>
2021-07-06 13:13:37 +0530

    Roman to decimal

---
+++ 450_Java/src/Strings/roman_to_decimal.java
@@ -0,0 +1,47 @@
+package Strings;
+
+public class roman_to_decimal {
+    public static int value(char r) {
+        if (r == 'I')
+            return 1;
+        if (r == 'V')
+            return 5;
+        if (r == 'X')
+            return 10;
+        if (r == 'L')
+            return 50;
+        if (r == 'C')
+            return 100;
+        if (r == 'D')
+            return 500;
+        if (r == 'M')
+            return 1000;
+        return -1;
+    }
+
+    public static int decimal(String s) {
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int s1 = value(s.charAt(i));
+            if (i + 1 < s.length()) {
+                int s2 = value(s.charAt(i + 1));
+                if (s1 >= s2) {
+                    result = result + s1;
+                } else {
+                    result = result + s2 - s1;
+                    i++;
+                }
+            } else {
+                result = result + s1;
+            }
+
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String str = "MCMIV";
+        System.out.print(decimal(str));
+    }
+}
