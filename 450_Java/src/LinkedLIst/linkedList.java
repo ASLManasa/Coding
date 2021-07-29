ff93195f1da208ed7990844f47b4129d0aa66d6f
ASLManasa <aslmanasa3@gmail.com>
2021-07-29 15:03:17 +0530

    linked list

---
+++ 450_Java/src/LinkedLIst/linkedList.java
@@ -0,0 +1,64 @@
+package LinkedLIst;
+
+import java.util.*;
+import java.util.LinkedList;
+
+public class linkedList {
+
+    class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public Node head = null;
+    public Node tail = null;
+
+    public void addNode(int data) {
+
+        Node newNode = new Node(data);
+        if (head == null) {
+
+            head = newNode;
+            tail = newNode;
+        } else {
+            //newNode will be added after tail such that tail's next will point to newNode
+            tail.next = newNode;
+            //newNode will become new tail of the list
+            tail = newNode;
+        }
+    }
+    public void display() {
+        Node current = head;
+
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        System.out.println("Nodes of singly linked list: ");
+        while (current != null) {
+
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        linkedList sList = new linkedList();
+
+        //Add nodes to the list
+        sList.addNode(1);
+        sList.addNode(2);
+        sList.addNode(3);
+        sList.addNode(4);
+
+        //Displays the nodes present in the list
+        sList.display();
+    }
+}
