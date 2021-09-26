c97c52856b400f5e5d9ba721b3fcd02332a7dbb6
ASLManasa <aslmanasa3@gmail.com>
2021-09-26 12:33:25 +0530

    Dp

---
+++ 450_Java/src/DynamicProgramming/CuttingRod.java
@@ -0,0 +1,8 @@
+package DynamicProgramming;
+
+public class CuttingRod {
+    public static void main(String [] args)
+    {
+
+    }
+}
--- 450_Java/src/DynamicProgramming/minimum_SubsetDifference.java
+++ 450_Java/src/DynamicProgramming/minimum_SubsetDifference.java
@@ -1,6 +1,49 @@
 package DynamicProgramming;
+//time complexity :o(n*sum)
 
-public class minimum_SubsetDifference
-{
+public class minimum_SubsetDifference {
+    public static int findMinRec(int arr[], int n, int k, int range) {
+        boolean dp[][] = new boolean[n + 1][range + 1];
+        for (int i = 0; i <= n; i++)
+            dp[i][0] = true;
+
+        for (int i = 1; i <= range; i++)
+            dp[0][i] = false;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= range; j++) {
+
+                dp[i][j] = dp[i - 1][j];
+
+
+                if (arr[i - 1] <= j)
+                    dp[i][j] |= dp[i - 1][j - arr[i - 1]];
+            }
+        }
+        int ans = Integer.MAX_VALUE;
+        int diff = 0;
+        for (int j = range / 2; j >= 0; j--) {
+            if (dp[n][j] == true) {
+                diff = range - 2 * j;
+                ans = Math.min(diff, ans);
+            }
+        }
+        return ans;
+    }
+
+    public static int findMin(int arr[], int n) {
+        int range = 0;
+        for (int i = 0; i < arr.length; i++) {
+            range = range + arr[i];
+
+        }
+        return findMinRec(arr, n, 0, range);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {3, 1, 4, 2, 2, 1};
+        int n = arr.length;
+        System.out.print("The minimum difference" + " between two sets is " + findMin(arr, n));
+    }
 
 }
