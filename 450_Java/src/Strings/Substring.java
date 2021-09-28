b04d7af5197406350ca2c672f714e9228cf9500e
ASLManasa <aslmanasa3@gmail.com>
2021-09-28 18:14:08 +0530

    Substrings

--- 450_Java/src/Strings/Substring.java
+++ 450_Java/src/Strings/Substring.java
@@ -2,9 +2,37 @@ package Strings;
 
 public class Substring {
 
-    public static void main(String [] args)
-    {
-        String s ="abc";
-//        substringString(s,s.length());
+    public static void substringString(char arr[], int n, String s) {
+  /*      1st-approach
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<=n;j++)
+            {
+                System.out.println(s.substring(i,j));
+            }
+        }
+        */
+
+//        2nd approach
+       for(int i=0;i<n;i++)
+        {
+            for(int j=i;j<n;j++)
+            {
+                for(int k=i;k<=j;k++)
+                {
+                    System.out.print(s.charAt(k));
+                }
+                System.out.println();
+            }
+        }
+
+
+
+    }
+
+    public static void main(String[] args) {
+        String s = "abcd";
+        char arr[] = s.toCharArray();
+        substringString(arr, s.length(), s);
     }
 }
